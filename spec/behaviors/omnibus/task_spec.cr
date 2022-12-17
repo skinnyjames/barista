a8c61694e8e7b2cba20f4bd6cf0b66ac18e5f5f4
Sean Gregory <sean.christopher.gregory@gmail.com>
2022-12-17 13:59:04 -0500

    Adds a spec around Task#relative_path

--- spec/behaviors/omnibus/task_spec.cr
+++ spec/behaviors/omnibus/task_spec.cr
@@ -2,6 +2,11 @@ require "../../spec_helper"
 
 private class TestProject < Barista::Project
   include Barista::Behaviors::Omnibus::Project
+
+  def initialize
+    install_dir(File.join(downloads_path, "install"))
+    barista_dir(File.join(downloads_path, "barista"))
+  end
 end
 
 @[Barista::BelongsTo(TestProject)]
@@ -13,11 +18,14 @@ private class ConfigurableTask < Barista::Task
   end
 
   def build : Nil
+    command("pwd")
+    command("cat test.txt")
   end
 
   def configure : Nil
     version("foobar")
-    source(File.join(fixture_url, "files", "test.tar.gz"))
+    relative_path(File.join("foo", "bar" ,"baz"))
+    source(File.join(fixture_url, "test.tar.gz"))
   end
 end
 
@@ -26,7 +34,19 @@ module Barista::Behaviors::Omnibus
     it "conifgures on initialize" do
       task = ConfigurableTask.new(TestProject.new)
       task.version.should eq("foobar")
-      task.source.try(&.uri.to_s).should eq(File.join(fixture_url, "files", "test.tar.gz"))
+      task.source.try(&.uri.to_s).should eq(File.join(fixture_url, "test.tar.gz"))
+    end
+
+    it "downloads the source to a relative path and executes commands in that context" do
+      output = [] of String
+      error = [] of String
+      ConfigurableTask.new(TestProject.new)
+        .collect_output(output)
+        .on_error { |str| puts str }
+        .execute
+
+      output[0].should contain("/foo/bar/baz")
+      output[1].should eq("foobar")
     end
   end
 end
